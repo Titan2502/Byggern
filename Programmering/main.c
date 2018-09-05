0faf84ed2d669c5ce8c4c5edaf2c6d8763f99d31
Titan2502 <espen_250293@hotmail.no>
2018-09-05 16:31:02 +0200

    Exercise 1 done, soon 2

--- Programmering/main.c
+++ Programmering/main.c
@@ -1,8 +1,18 @@
+#include "F_CPU.h"
 #include <avr/io.h>
+#include "util/delay.h"
+#include "uart.h"
+#include "accessing_memory_test.h"
 
 int main()
 {
+  USART_Init();
+  MCUCR |= (1<<SRE);
+  SRAM_test();
+
   while(1){
-    USART_Init ( MYUBRR );
+    // USART_Transmit('G', NULL);
+    // printf( "HELLOOOOOOOOOOOOOO\n" );
+
   }
 }
